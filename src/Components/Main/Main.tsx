19fe8232bd4e7979ef845823890810875a5f59de
Pawel Rybka <rrybka1999@gmail.com>
2023-04-20 11:00:11 +0200

    Add loadingSpinner into project

--- src/Components/Main/Main.tsx
+++ src/Components/Main/Main.tsx
@@ -2,6 +2,7 @@ import Footer from '../Footer/Footer'
 import { useState, useEffect } from 'react';
 import { MdOutlineKeyboardArrowDown } from 'react-icons/md';
 import { MdOutlineKeyboardArrowUp } from 'react-icons/md';
+import LoadingSpinner from '../../UI/LoadingSpinner/LoadingSpinner';
 import styles from './Main.module.css'
 
 interface Products {
@@ -14,14 +15,15 @@ interface Products {
 
 function Main() {
   
+  const [loading, setLoading] = useState(true);
   const [products, setProducts] = useState<Products[]>([])
 
   useEffect(() => {
     async function fetchData() {
       const response = await fetch('https://fakestoreapi.com/products');
       const data = await response.json();
-      console.log(data);
       setProducts(data)
+      setLoading(false);
     }
     fetchData()
   }, []);
@@ -30,26 +32,29 @@ function Main() {
     <div className={styles.main}>
       <div className={styles.main__content}>
         <div className={styles.content}>
-          {products.map(product => (
-            <div key={product.id} className={styles.product}>
-              <div className={styles.product__info}>
-                <img 
-                src={product.image} 
-                alt={product.category} 
-                className={styles.product__image}
-                />
-                <div>
-                  <h3>{product.category}</h3>
-                  <p>{product.price}$</p>
+          {loading ? 
+            <LoadingSpinner/> : 
+            products.map(product => (
+              <div key={product.id} className={styles.product}>
+                <div className={styles.product__info}>
+                  <img 
+                  src={product.image} 
+                  alt={product.category} 
+                  className={styles.product__image}
+                  />
+                  <div>
+                    <h3>{product.category}</h3>
+                    <p>{product.price}$</p>
+                  </div>
+                </div>
+                <div className={styles.product__quantity}>
+                  <MdOutlineKeyboardArrowUp size={30}/>
+                    0
+                  <MdOutlineKeyboardArrowDown size={30}/>
                 </div>
               </div>
-              <div className={styles.product__quantity}>
-                <MdOutlineKeyboardArrowUp size={30}/>
-                  0
-                <MdOutlineKeyboardArrowDown size={30}/>
-              </div>
-            </div>
-          ))}
+            ))
+          }
         </div>
         <Footer/>
       </div>
---
+++ src/UI/LoadingSpinner/LoadingSpinner.tsx
@@ -0,0 +1,18 @@
+import styles from './LoadingSpinner.module.css'
+
+function LoadingSpinner() {
+  return (
+    <div className={styles.ldsroller}>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+  )
+}
+
+export default LoadingSpinner;
