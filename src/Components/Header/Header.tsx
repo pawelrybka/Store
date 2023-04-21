52b886b67d715264bd5db11a93fe6e3f411ac2a0
Pawel Rybka <rrybka1999@gmail.com>
2023-04-21 10:02:28 +0200

    Basket quantity work

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,14 +1,20 @@
-import styles from './Header.module.css'
 import { BsFillBasketFill } from 'react-icons/bs';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../Redux/Store';
+import styles from './Header.module.css';
 
 function Header() {
+  
+  const quantities = useSelector((state: RootState) => state.quantity);
+  const totalQuantity = Object.values(quantities).reduce((acc, curr) => acc + curr, 0);
+  
   return (
     <div className={styles.header}>
       <div className={styles.header__content}>
         <h2>Logo</h2>
         <div className={styles.basket}>
           <BsFillBasketFill size={30}/>
-          <div className={styles.basket__counter}>0</div>
+          <div className={styles.basket__counter}>{totalQuantity}</div>
         </div>
       </div>
     </div>
